9adc7e365c8e37693844c48d14b7902a7a7bc1e5
Kumbb <152781721+Kumbb@users.noreply.github.com>
2023-12-03 18:56:40 +0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,16 @@
+const wrapper document.querySelector(".wrapper");
+const question document.querySelector(".question");
+const gif document.querySelector(".gif"); const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () {
+question.innerHTML = "Aaaaa, I like you too";
+"https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; });
+
+gif.src=
+noBtn.addEventListener("mouseover", () {
+const noßtnRect = noßtn.getBoundingClientRect(); noßtnRect.width;
+const maxX = window.innerWidth const maxy window.innerHeight noBtnRect.height;
+const randomX = Math.floor(Math.random() maxx);
+const randomy = Math.floor(Math.random() maxY);
+noBtn.style.left noßtn.style.top }); randomX + "px"; randomy + "px";
